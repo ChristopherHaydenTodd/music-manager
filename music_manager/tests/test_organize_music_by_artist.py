58775a4dd3ee1632c600abdf39b79782be62267d
Christopher H. Todd <Christopher.Hayden.Todd@gmail.com>
2019-05-22 22:34:23 -0400

    adding stub organize music script

---
+++ music_manager/tests/test_organize_music_by_artist.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""
+    Purpose:
+        Test File for organize_music_by_artist.py
+"""
+
+# Python Library Imports
+import os
+import sys
+import pytest
+from unittest import mock
+
+# Import File to Test
+from music_manager import organize_music_by_artist
+
+
+###
+# Fixtures
+###
+
+
+# None at the Moment (Empty Test Suite)
+
+
+###
+# Mocked Functions
+###
+
+
+# None at the Moment (Empty Test Suite)
+
+
+###
+# Test Payload
+###
+
+
+# None at the Moment (Empty Test Suite)
